a173c5e8fa17794433befe9374102b8b24b5e9f7
wlstjdgns <jin009471@naver.com>
2023-07-28 16:00:47 +0900

    최초커밋

---
+++ src/main/java/shop/mtcoding/blogv5/testBlog/HttpControllerTest.java
@@ -0,0 +1,33 @@
+package shop.mtcoding.blogv5.testBlog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//사용자가 요청 -> 응답(HTML 파일)
+//@controller
+@RestController // 사용자가 요청 -> 응답(Data)
+public class HttpControllerTest {
+	
+	//http://localhost:8080/http/post(select)
+	@GetMapping("/http/get")
+	public String getTest() {
+		return "get 요청";
+	}
+	//http://localhost:8080/http/post(insert)
+	@PostMapping("/http/post")
+	public String postTest() {
+		return "post 요청";
+	}
+	//http://localhost:8080/http/post(update)
+	@GetMapping("/http/put")
+	public String putTest() {
+		return "put 요청";
+	}
+	//http://localhost:8080/http/post(delete)
+	@GetMapping("/http/deleteTest")
+	public String deleteTest() {
+		return "delete 요청";
+	}
+
+}
